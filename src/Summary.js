071c792793f8cfd62eb0729de2427cd6ba59de27
unknown <mp1845071@gmail.com>
2024-03-29 08:13:01 +0530

    My Resume

---
+++ src/Summary.js
@@ -0,0 +1,14 @@
+function Summary() {
+    return (
+      <section className="Summary">
+        <h2>Summary</h2>
+        <div>
+          <p>2018<br/>
+          Financial Accounting<br/>
+          Oxford Software institute India</p>
+        </div>
+      </section>
+    );
+  }
+  
+  export default Summary;
